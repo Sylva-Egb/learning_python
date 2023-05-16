ce71abe0051b3358c009dfcbd72245af46b98b4e
Sylva-Egb <romaricegbewole@gmail.com>
2023-05-16 12:20:39 +0200

    printing to screen and reading data from the user

---
+++ name.py
@@ -0,0 +1,10 @@
+firstname = input('enter your firstname please\n');
+lastname = input('enter your lastname please\n');
+profession = input('enter your profession please\n');
+
+print('So you enter this information:\n' +
+ 'Firstname: ' + firstname + '\n'+
+ 'Lastname: ' + lastname + '\n'+ 
+ 'Great to see that you\'re ' + profession
+  )
+input('press any key to exit the program')
